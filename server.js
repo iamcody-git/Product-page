190c9ae71d2b870495b05bb0b9fea2017e485f8c
iamcody-git <iamcody.github@gmail.com>
2024-12-11 21:40:50 +0545

    modified for production

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ import dotenv from 'dotenv';
 import { connectDB } from './Config/db.js';
 import productRoutes from './Routes/productRoutes.js';
 import cors from 'cors'
+import path from 'path'
 
 dotenv.config(); 
 
 const app = express();
 const port = process.env.PORT || 4000;
 
+const __dirname = path.resolve()
+
 app.use(cors());
 app.use(express.json());
 app.use('/api/products', productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/Frontend/dist")))
+
+}
+
 
 app.listen(port, () => {
     connectDB(); 
